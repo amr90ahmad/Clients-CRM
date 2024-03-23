36c2c19ff0fd3efa7a450efc54627d32c4b12016
amr099 <amr70210@gmail.com>
2024-03-23 19:10:05 +0200

    fix logout

--- src/components/logout.tsx
+++ src/components/logout.tsx
@@ -3,9 +3,12 @@ import { Button } from "@/components/ui/button";
 import { signOut } from "next-auth/react";
 
 export default function Logout() {
+    const out = async () => {
+        await signOut();
+    };
     return (
         <>
-            <Button className='' onClick={signOut} variant='secondary'>
+            <Button className='' onClick={out} variant='secondary'>
                 Logout
             </Button>
         </>
